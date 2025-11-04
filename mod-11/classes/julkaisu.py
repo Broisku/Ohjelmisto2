20492eefa17a18b6d5a2667f82ea43fb98dd50b7
Broisku <miro.o.heiskanen@gmail.com>
2025-11-04 22:36:44 +0200

    t1

--- mod-11/classes/julkaisu.py
+++ mod-11/classes/julkaisu.py
@@ -2,8 +2,25 @@ class Julkaisu:
     def __init__(self, nimi):
         self.nimi = nimi
 
+    def tulosta_tiedot(self):
+        print(f"Nimi: {self.nimi}")
+
 class Kirja(Julkaisu):
     def __init__(self, nimi, kirjoittaja, sivumaara):
         self.kirjoittaja = kirjoittaja
         self.sivumaara = sivumaara
-        super().__init__(kirjoittaja, sivumaara)
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Kirjoittaja: {self.kirjoittaja}, Sivumäärä: {self.sivumaara}")
+
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        self.paatoimittaja = paatoimittaja
+        super().__init__(nimi)
+
+    def tulosta_tiedot(self):
+        super().tulosta_tiedot()
+        print(f"Päätoimittaja: {self.paatoimittaja}")
---
+++ mod-11/t1.py
@@ -0,0 +1,16 @@
+from classes.julkaisu import Julkaisu, Lehti, Kirja
+
+p1 = "Aku Ankka"
+p2 = "Aki Hyyppä"
+
+lehti = Lehti(p1, p2)
+
+p3 = "Hytti n:o 6"
+p4 = "Rosa Liksom"
+p5 = 200
+
+kirja = Kirja(p3, p4, p5)
+
+lehti.tulosta_tiedot()
+print("")
+kirja.tulosta_tiedot()
